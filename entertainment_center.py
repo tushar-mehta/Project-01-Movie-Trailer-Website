17a7909c53fe863b8672d96df26f34ed25803159
Tushar Mehta <tushar.mehta.code@gmail.com>
2016-06-22 00:40:31 +0800

    shows thumbnails and modal on clicking poster image

--- entertainment_center.py
+++ entertainment_center.py
@@ -9,17 +9,36 @@ from fresh_potato import open_movies_page
 
 def main():
     movies = [media.Movie('X-Men: Apocalypse',
-                          'http://heroichollywood.com/wp-content/uploads/2016/04/maxresdefault-2.jpg',  # noqa
+                          'http://placehold.it/250x300',  # noqa
                           'https://www.youtube.com/watch?v=N0io2w_6vT8'),
               media.Movie('X-Men: Apocalypse',
-                                    'http://heroichollywood.com/wp-content/uploads/2016/04/maxresdefault-2.jpg',  # noqa
+                                    'http://placehold.it/250x300',  # noqa
                                     'https://www.youtube.com/watch?v=N0io2w_6vT8'),
               media.Movie('X-Men: Apocalypse',
-                                    'http://heroichollywood.com/wp-content/uploads/2016/04/maxresdefault-2.jpg',  # noqa
+                                    'http://placehold.it/250x300',  # noqa
                                     'https://www.youtube.com/watch?v=N0io2w_6vT8'),
               media.Movie('X-Men: Apocalypse',
-                                    'http://heroichollywood.com/wp-content/uploads/2016/04/maxresdefault-2.jpg',  # noqa
-                                    'https://www.youtube.com/watch?v=N0io2w_6vT8')]
+                                    'http://placehold.it/250x300',  # noqa
+                                    'https://www.youtube.com/watch?v=N0io2w_6vT8'),
+              media.Movie('X-Men: Apocalypse',
+                                    'http://placehold.it/250x300',  # noqa
+                                    'https://www.youtube.com/watch?v=N0io2w_6vT8'),
+              media.Movie('X-Men: Apocalypse',
+                                    'http://placehold.it/250x300',  # noqa
+                                    'https://www.youtube.com/watch?v=N0io2w_6vT8'),
+              media.Movie('X-Men: Apocalypse',
+                                    'http://placehold.it/250x300',  # noqa
+                                    'https://www.youtube.com/watch?v=N0io2w_6vT8'),
+              media.Movie('X-Men: Apocalypse',
+                                    'http://placehold.it/250x300',  # noqa
+                                    'https://www.youtube.com/watch?v=N0io2w_6vT8'),
+              media.Movie('X-Men: Apocalypse',
+                                    'http://placehold.it/250x300',  # noqa
+                                    'https://www.youtube.com/watch?v=N0io2w_6vT8'),
+              media.Movie('X-Men: Apocalypse',
+                                    'http://placehold.it/250x300',  # noqa
+                                    'https://www.youtube.com/watch?v=N0io2w_6vT8')
+            ]
 
     # pass list of movies to be displayed in the webpage
     open_movies_page(movies)
--- fresh_potato.py
+++ fresh_potato.py
@@ -4,14 +4,14 @@ import re
 
 movie_detail = '''
 <div class="col-md-4">
-    <img src="{movie_poster_url}">
-    <h3>{movie_title}</h3>
-    <h2>{trailer_youtube_url}</h2>
+    <img src="{movie_poster_url}" data-toggle="modal" data-target="#{modal_id}" class="img-responsive center-block img-thumbnail" alt="Responsive image">
+    <h4>{movie_title}</h4>
 </div>
 '''
 
 movie_row = '''
 <div class="row text-center">
+    {row_sep}
     {movies}
 </div>
 '''
@@ -23,11 +23,6 @@ first_row = '''
 <div class="row">
     <h1>Other Information</h1>
 </div>
-<div class="row">
-    <div class="col-md-12">
-        <hr>
-    </div>
-</div>
 '''
 
 row_seperator = '''
@@ -38,11 +33,13 @@ row_seperator = '''
 </div>
 '''
 
-page_head = '''
+head = '''
 <head>
     <meta charset="utf-8">
-    <title>Hello World</title>
+    <title>Fresh Patato!</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
 '''
 
@@ -51,7 +48,7 @@ page_body = '''
     <div class="container">
         {row1}
         {mov_rows}
-        {row_sep}
+        {modals}
     </div>
 </body>
 '''
@@ -64,35 +61,71 @@ page = '''
 </html>
 '''
 
+modal = '''
+<!-- Modal -->
+<div class="modal fade" id="{modal_id}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+<div class="modal-dialog" role="document">
+<div class="modal-content">
+    <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">{movie_title}</h4>
+    </div>
+    <div class="modal-body">
+        ...
+    </div>
+    <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+    </div>
+</div>
+</div>
+</div>
+'''
+
 
 def create_movie_tiles_content(movies):
     # The HTML content for this section of the page
     rows = ''
     movies_tuples = [movies[i:i+3] for i in range(0, len(movies), 3)]
+    module_no = 0
     for movie_tuple in movies_tuples:
         row_data = ''
         for movie in movie_tuple:
+            module_no += 1
             row_data += movie_detail.format(movie_poster_url=movie.poster_image_url,
                                             movie_title=movie.title,
-                                            trailer_youtube_url=movie.trailer_youtube_url)
+                                            modal_id='modal_{}'.format(module_no))
 
-        rows += movie_row.format(movies=row_data)
+        rows += movie_row.format(row_sep=row_seperator, movies=row_data)
 
     return rows
 
 
+def create_movie_modals(movies):
+    modals = ''
+    for index, movie in enumerate(movies):
+        modals += modal.format(modal_id='modal_{}'.format(index), movie_title=movie.title)
+
+    return modals
+
+
 def open_movies_page(movies):
     # Create or overwrite the output file
-    output_file = open('fresh_potato.html', 'w')
+    try:
+        output_file = open('fresh_potato.html', 'w')
+    except Exception as e:
+        print(e)
 
-    page_body.format(row1=first_row,
-                     mov_rows=create_movie_tiles_content(movies),
-                     row_sep=row_seperator)
+    modals = create_movie_modals(movies)
 
-    page.format(head=page_head, body=page_body)
+    body = page_body.format(row1=first_row,
+                            mov_rows=create_movie_tiles_content(movies),
+                            modals=modals)
+
+    html_page = page.format(head=head, body=body)
 
     # Output the file
-    output_file.write(page)
+    output_file.write(html_page)
     output_file.close()
 
     # open the output file in the browser (in a new tab, if possible)
