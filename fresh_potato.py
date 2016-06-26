7ef59280efa56fea2a690a38b3791701c6c5c13b
Tushar Mehta <tushar.mehta.code@gmail.com>
2016-06-26 11:46:33 +0800

    added css and .js local files

--- fresh_potato.py
+++ fresh_potato.py
@@ -142,7 +142,7 @@ body = '''
     {modals}
     {video_generic_modal}
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
     <script>{script}</script>
 </body>
 '''
